6b7a4167f12c2dbce54a78b8026aab7ade140262
sv-004 <ma_shihao@yeah.net>
2016-06-01 16:22:27 +0800

    2.第二次提交,添加了一个Button的点击事件

--- app/src/main/java/git/gittest/MainActivity.java
+++ app/src/main/java/git/gittest/MainActivity.java
@@ -15,9 +15,9 @@ public class MainActivity extends AppCompatActivity {
     public void testgit(View view) {
         Toast.makeText(MainActivity.this, "Test Git1", Toast.LENGTH_SHORT).show();
     }
-//    public void testgit2(View view) {
-//        Toast.makeText(MainActivity.this, "Test Git2", Toast.LENGTH_SHORT).show();
-//    }
+    public void testgit2(View view) {
+        Toast.makeText(MainActivity.this, "Test Git2", Toast.LENGTH_SHORT).show();
+    }
 //    public void testgit3(View view) {
 //        Toast.makeText(MainActivity.this, "Test Git3", Toast.LENGTH_SHORT).show();
 //    }
